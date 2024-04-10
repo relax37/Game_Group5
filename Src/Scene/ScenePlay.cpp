2cc7d20b6ffe3cfbd3fa8efc762a5672cc79c535
shoya <144639871+relax37@users.noreply.github.com>
2024-04-10 15:29:49 +0900

    totyuu

--- Src/Scene/ScenePlay.cpp
+++ Src/Scene/ScenePlay.cpp
@@ -2,10 +2,28 @@
 #include "Scene.h"
 #include "ScenePlay.h"
 
-ScenePlay::ScenePlay() 
+ScenePlay::ScenePlay()
 {
 	// プレイ背景ハンドル
 	PlayBGHandle = 0;
+
+	for (int WoodIndex = 0; WoodIndex < WOOD_MAX_NUM; WoodIndex++)
+	{
+		// 木の画像ハンドル
+		WoodImgHandle[WoodIndex] = 0;
+
+		// 木の座標
+		WoodPosX[WoodIndex] = WoodPosY[WoodIndex] = 0;
+	}
+
+	for (int TwigIndex = 0; TwigIndex < TWIG_MAX_NUM; TwigIndex++)
+	{
+		// 枝の画像ハンドル
+		TwigImgHandle[TwigIndex] = 0;
+
+		// 枝の座標
+		TwigPosX[TwigIndex] = TwigPosY[TwigIndex] = 0;
+	}
 }
 ScenePlay::~ScenePlay() { FinPlay(); }
 
@@ -14,6 +32,24 @@ void ScenePlay::InitPlay()
 {
 	// プレイ背景ハンドル
 	PlayBGHandle = LoadGraph(PLAY_BG_PATH);
+
+	for (int WoodIndex = 0; WoodIndex < WOOD_MAX_NUM; WoodIndex++)
+	{
+		// 木の画像ハンドル
+		WoodImgHandle[WoodIndex] = LoadGraph(WOOD_IMG_PATH);
+
+		// 木の座標
+		WoodPosX[WoodIndex] = WoodPosY[WoodIndex] = 0;
+	}
+
+	for (int TwigIndex = 0; TwigIndex < TWIG_MAX_NUM; TwigIndex++)
+	{
+		// 枝の画像ハンドル
+		TwigImgHandle[TwigIndex] = LoadGraph(TWIG_IMG_PATH);
+
+		// 枝の座標
+		TwigPosX[TwigIndex] = TwigPosY[TwigIndex] = 0;
+	}
 
 	g_CurrentSceneID = SCENE_ID_LOOP_PLAY;
 }
@@ -35,6 +71,21 @@ void ScenePlay::DrawPlay()
 {
 	// タイトル背景描画
 	DrawGraph(0, 0, PlayBGHandle, true);
+
+	for (int WoodIndex = 0; WoodIndex < WOOD_MAX_NUM; WoodIndex++)
+	{
+		// 木の画像ハンドル
+		DrawRotaGraph(WoodPosX[WoodIndex], WoodPosY[WoodIndex] * WoodIndex + WOOD_SIZE / 2, 1.0f, 0.0f, false, false);
+	}
+
+	for (int TwigIndex = 0; TwigIndex < TWIG_MAX_NUM; TwigIndex++)
+	{
+		// 枝の画像ハンドル
+		TwigImgHandle[TwigIndex] = LoadGraph(TWIG_IMG_PATH);
+
+		// 枝の座標
+		TwigPosX[TwigIndex] = TwigPosY[TwigIndex] = 0;
+	}
 }
 
 //プレイシーン終了処理
--- Src/Scene/ScenePlay.h
+++ Src/Scene/ScenePlay.h
@@ -4,10 +4,24 @@
 // プレイ背景
 #define PLAY_BG_PATH "Data/Image/BackGround/TentativePlayBG.png"
 
+// 木
+#define WOOD_IMG_PATH "Data/Imaga/Play/karinoki.png"
+#define WOOD_MAX_NUM (100)
+#define WOOD_SIZE (128)
+
+// 枝
+#define TWIG_IMG_PATH "Data/Imaga/Play/karinoeda.png"
+#define TWIG_MAX_NUM (50)
+#define TWIG_SIZE (128)
+
 class ScenePlay {
 private:
-	// プレイ背景ハンドル
-	int PlayBGHandle;
+	int PlayBGHandle;									// プレイ背景ハンドル
+	int WoodImgHandle[WOOD_MAX_NUM];					// 木の画像ハンドル
+	int TwigImgHandle[TWIG_MAX_NUM];					// 枝の画像ハンドル
+
+	int WoodPosX[WOOD_MAX_NUM], WoodPosY[WOOD_MAX_NUM];	// 木の座標
+	int TwigPosX[TWIG_MAX_NUM], TwigPosY[TWIG_MAX_NUM];	// 枝の座標
 
 public:
 	ScenePlay();
